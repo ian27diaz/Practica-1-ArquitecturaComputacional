1d6f742d46258c5bd8ba1254dbe263c62cc10526
Ian27Diaz <anand_ian@hotmail.com>
2019-02-28 12:38:29 -0600

    Contadores para las torres y bug fixing

--- TorresHanoi.asm
+++ TorresHanoi.asm
@@ -5,7 +5,7 @@
 .text
 
 main:
-	addi $s0, $zero, 3 #Number of discs
+	addi $s0, $zero, 2 #Number of discs
 	#initializing pointers
 	addi $s5, $zero,0x1001 #s5 -> origin tower
 	sll $s5, $s5, 16
@@ -26,7 +26,13 @@ loadDiscs:
 	addi, $t0, $t0, -1
 	bne $t0, $zero, loop_LD
 	
-	add $s5, $s5, -4
+	#contadores de discos, servirán para manejar apuntadores
+	add $s2, $zero, $s0   #Orig Tower count -> s5
+	add $s3, $zero, $zero #Dest Tower count -> s6
+	add $s4, $zero, $zero #Aux  Tower count -> s7
+	########################################################
+	
+	add $s5, $s5, -4 #Para que no apunte a un elemento vacio 
 	
 	#prepping arguments of Hanoi
 	add $a0, $zero, $s0 
@@ -45,61 +51,109 @@ Hanoi:
 	sw $a0, 0($sp)
 	sw $t8, 4($sp)
 	sw $ra, 8($sp)
-	add $t0, $zero, $a0
-	beq $t0, 1, baseCase #if (n == 1) { baseCase}
-	casoInductivo:
+	beq $a0, 1, baseCase #if (n == 1) { baseCase}
+	#casoInductivo:
 	
 	#else {
-<<<<<<< HEAD
 	#Hanoi ( n - 1, org, dest, aux)
 	#Move disc from org to dest
 	#Hanoi (n - 1, aux, dest, org)
 	#}
+
+	addi $a0,$a0,-1 # n = n - 1
+	sw $a0, 0($sp)
 	
-	addi $a0,$a0,-1
-	#hanoi(n-1,org,aux,dest)
 	#en este primer paso del caso en esta llamada el origen siempre es el mismo pero cambios en la torres aux y dest
 	#en cada llamada aux será dest y dest será aux variando en los casos en que llame esta función
-	add $t7,$zero,$a2
-	add $a2,$zero,$a3
-	add $a3,$zero,$t7
-	
+	#n => #a0
+	#Orig Tower count -> s2 (De s5)
+	#Dest Tower count -> s3 (De s6)
+	#Aux  Tower count -> s4 (De s7)
+
+	#Cambiando apuntadores de pilas
+	add $t7,$zero,$a2 # t7  = destino
+	add $a2,$zero,$a3 #dest = aux
+	add $a3,$zero,$t7 #aux  = destino
+	#Cambiando contadores de pilas
+	#add $t7, $zero, $s3 #t7 = contador de pila destino
+	#add $s3, $zero, $s4 #s2(destino) = contador de pila aux
+	#add $s4, $zero, $t7 #s4(aux) = contador de pila destino
+	#Llamando a Hanoi(n-1,org,aux,dest)
 	jal Hanoi
+	lw $a0, 0($sp)
+	lw $t8, 4($sp)
+	lw $ra, 8($sp)
 	
+	add $t7, $zero, $a2 #ty = aux
+	add $a2, $zero, $a3 #aux = dest
+	add $a3, $zero, $t7 #dest = aux
+	
+	#add $t7, $zero, $s3 #t7 = contador de pila aux
+	#add $s3, $zero, $s4 #s2(aux) = contador de pila destino
+	#add $s4, $zero, $t7 #s4(dest) = contador de pila aux
 	#dest.push(o.pop());
 	lw $t8,0($a1) # $t8 = o.pop();
 	sw $zero,0($a1)
-	addi $a1,$a1,-4 
-	sw $t8,0($a2)#d.push($t8);
-	addi $a2,$a2,4	
+	addi $a1,$a1,-4
+	bne $s2, 1, afterOrigFix
+	addi $a1, $a1, 4
 	
+	afterOrigFix:
+	add $s2, $s2, -1
+	sw $t8,0($a2)#d.push($t8);
+	addi $a2,$a2,4
+	bne $s3, 0, afterDestFix #Si la pila de destino esta vacia...
+	addi $a2,$a2,-4
+	
+	afterDestFix:
+	add $s3, $s3, 1
+	sw $a0, 0($sp)
+	sw $t8, 4($sp)
+	sw $ra, 8($sp)
 	#en este paso del caso en esta llamada el dest siempre es el mismo pero habrá cambios en las torres aux y orig
 	#en cada llamada aux será orig y origin será aux variando en los casos en que llame esta función
-	add $t7,$zero,$a1
-	add $a1,$zero,$a3
-	add $a3,$zero,$t7
+	add $t7,$zero,$a1 #t7 = org
+	add $a1,$zero,$a3 #org = aux
+	add $a3,$zero,$t7 #aux = org
 	
-	jal Hanoi
-	#hanoi(n-1,aux,dest,origin)
+	#add $t7, $zero, $s2 #t7 = contador de pila orig
+	#add $s2, $zero, $s4 #s2(orig) = contador de pila aux
+	#add $s4, $zero, $t7 #s4(aux) = contador de pila org
+	
+	jal Hanoi #hanoi(n-1,aux,dest,origin)
+	add $t7, $zero, $a1 #ty = aux
+	add $a1, $zero, $a3 #aux = dest
+	add $a3, $zero, $t7 #dest = aux
+	
+	#add $t7, $zero, $s2 #t7 = contador de pila orig
+	#add $s2, $zero, $s4 #s2(orig) = contador de pila aux
+	#add $s4, $zero, $t7 #s4(aux) = contador de pila org
+	
 	j endHanoi
-	
-	
-	
-	
+
 	baseCase:
 	#Mov disc from orig to dest
 	lw $t8,0($a1) # $t8 = o.pop();
 	sw $zero,0($a1)
-	addi $a1,$a1,-4 
+	addi $a1,$a1,-4
+	bne $s2, 1, afterOrigFix2
+	addi $a1, $a1, 4
+	
+	afterOrigFix2:
+	add $s2, $s2, -1
 	sw $t8,0($a2)#d.push($t8);
-	addi $a2,$a2,4	
-
-	endHanoi:	
+	addi $a2,$a2,4
+	bne $s3, 0, afterDestFix2 #Si la pila de destino esta vacia...
+	addi $a2,$a2,-4
+	
+	afterDestFix2:
+	add $s3, $s3, 1
+	endHanoi:
 	#terminando Hanoi
 	lw $a0, 0($sp)
 	lw $t8, 4($sp)
 	lw $ra, 8($sp)
-	addi $sp, $sp, -12
+	addi $sp, $sp, 12
 	jr $ra	
 	
 
